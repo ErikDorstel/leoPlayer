c983138bb9ddc70a16fc9042837b793da87e0584
Erik Dorstel <43003063+ErikDorstel@users.noreply.github.com>
2024-11-18 16:31:35 +0100

    improved battery management

--- battery.h
+++ battery.h
@@ -1,5 +1,6 @@
 #define batteryMin 3.5f
 #define batteryMax 4.2f
+#define batteryChargeMax 4.3f
 
 struct { float battery[2]={10,10}; uint8_t gauge=10; } batteryControl;
 
@@ -12,10 +13,14 @@ void initBattery() {
   pinMode(4,OUTPUT); digitalWrite(4,LOW); // Battery Charger Control
   pinMode(27,INPUT); } // 5 Volt from USB C
 
+bool getUsbcStatus() { return digitalRead(27); }
+
 void getGauge() {
   float battery=min(batteryControl.battery[0],batteryControl.battery[1]);
-  if (battery<batteryMin) { batteryControl.gauge=0; }
-  else { batteryControl.gauge=(battery-batteryMin)/(batteryMax-batteryMin)*8.0f+1.0f; } }
+  if (battery<batteryMin) { batteryControl.gauge=0; return; }
+  battery=(batteryControl.battery[0]+batteryControl.battery[1])/2.0f;
+  if (getUsbcStatus()) { batteryControl.gauge=(battery-batteryMin)/(batteryChargeMax-batteryMin)*7.0f+1.0f; }
+  else { batteryControl.gauge=(battery-batteryMin)/(batteryMax-batteryMin)*7.0f+1.0f; } }
 
 void checkBattery() {
   static uint64_t batteryTimer=0;
@@ -32,8 +37,6 @@ void checkBattery() {
     rawBattery[0]=0;
     rawBattery[1]=0;
     getGauge(); } else { count++; } }
-
-bool getUsbcStatus() { return digitalRead(27); }
 
 void switchOff() {
   digitalWrite(4,HIGH); delay(100);
